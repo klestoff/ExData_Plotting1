3662c748e1a8db4485df066bfc1c6bb583ef6774
Alexander A. Klestov <alan@klestoff.ru>
2015-11-09 00:50:35 +0300

    plot 3 completed

---
+++ plot3.R
@@ -0,0 +1,23 @@
+source("functions.R")
+
+data <- readData("household_power_consumption.txt")
+
+png(filename = "plot3.png", width = 400, height = 400, bg = "transparent")
+plot(
+    data$DT,
+    data$Sub_metering_1,
+    type = "n",
+    xlab = "",
+    ylab = "Energy sub mettering"
+)
+lines(data$DT, data$Sub_metering_1, type = "l", col = "black")
+lines(data$DT, data$Sub_metering_2, type = "l", col = "red")
+lines(data$DT, data$Sub_metering_3, type = "l", col = "blue")
+legend(
+    "topright",
+    legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+    col = c("black", "red", "blue"),
+    lty = 1,
+    lwd = 1
+)
+dev.off()
